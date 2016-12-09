21f900afdb6b362c4305668e38731e10708e42df
Brian Olsen <brian@maven-group.org>
2016-12-09 16:43:40 +0100

    Update windows build product copy command

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,4 @@ GOOS=linux   GOARCH=amd64 go-wrapper install
 GOOS=windows GOARCH=386   go-wrapper install
 mkdir -p dist/linux_amd64/
 cp /go/bin/teamcity-steps dist/linux_amd64/
-cp -R /go/bin/windows_386/ dist/windows_386/
+cp -R /go/bin/windows_386/* dist/windows_386/
