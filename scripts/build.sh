1bae947bd2496f1793030033b8e4c9ddc7f6507e
Brian Olsen <brian@maven-group.org>
2016-12-09 16:46:40 +0100

    Make windows build product directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,5 @@ GOOS=linux   GOARCH=amd64 go-wrapper install
 GOOS=windows GOARCH=386   go-wrapper install
 mkdir -p dist/linux_amd64/
 cp /go/bin/teamcity-steps dist/linux_amd64/
+mkdir -p dist/windows_386/
 cp -R /go/bin/windows_386/* dist/windows_386/
